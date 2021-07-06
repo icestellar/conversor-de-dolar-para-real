66f32e8c24b8eb9045f5f8e9f738ba7073e32f65
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-06 19:38:29 -0400

    Removido implementações desnecessárias

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,23 +1,20 @@
-import React, { Component} from 'react';
 import { ContainerDiv, Img, ContainerText } from './Style';
 import Logo from "../../Assets/Images/Logo.svg"
+
 interface  iProps{
     day: string, 
     hour: string
 }
-class Header extends Component<iProps>{
-
-  render() {
+const Header = (props:iProps) =>{
     return (
         <ContainerDiv>
             <Img src={Logo}></Img>
             <ContainerText>
-                <p className="main-text">{this.props.day} &emsp;| &emsp; {this.props.hour} UTC</p>
+                <p className="main-text">{props.day} &emsp;| &emsp; {props.hour} UTC</p>
                 <p className="info-text"> Dados de câmbio disponibilizados pela Morningstar.</p>
             </ContainerText>
         </ContainerDiv>
       );
     };
-}
 
 export default Header;
