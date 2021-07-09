d100e0c6a4a21b6a836e2e12a6f4ce7539365adf
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-08 23:04:08 -0400

    Adicionado responsividade no DolarForm

--- src/Components/DollarForm/DollarForm.tsx
+++ src/Components/DollarForm/DollarForm.tsx
@@ -1,14 +1,17 @@
 import { TextField } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import PaymentMethod from '../PaymentMethod/PaymentMethod';
-import ConvertButton from '../Buttons/ConvertButton';
-interface iProps{
-  dollarAmount: any, 
+import { ConvertButton } from '../Buttons/Buttons';
+import { FormBox } from '../DollarForm/Style';
+
+interface iProps {
+  dollarAmount: any,
   percentage: any,
-  paymentMethod: string, 
-  convertClicked: boolean, 
-  paymentMethodHandler: Function, 
+  paymentMethod: string,
+  convertClicked: boolean,
+  paymentMethodHandler: Function,
   dollarAmountHandler: Function,
   percentageHandler: Function,
   convertClickedHandler: Function
@@ -34,44 +37,56 @@ const GreenTextField = withStyles({
     },
   },
 })(TextField);
-const DollarForm = (props:iProps) => {
-  return(
+const DollarForm = (props: iProps) => {
+  return (
     <>
-    <form>
-      <GreenTextField
-        variant="outlined"
-        margin="normal"
-        name="dollarAmount"
-        label="Dólar"
-        id="dollarAmount"
-        autoFocus
-        required
-        type="number"
-        value={props.dollarAmount}
-        onChange={(event) => {props.dollarAmountHandler(event.target.value)}}
-        onBlur={(event) => {props.dollarAmountHandler(Number(event.target.value).toFixed(2))}}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>
-        }}
-      />
-      <GreenTextField
-        variant="outlined"
-        margin="normal"
-        name="taxState"
-        required
-        label="Taxa do estado"
-        id="password"
-        type="number"
-        value={props.percentage}
-        onChange={(event) => {props.percentageHandler(event.target.value)}}
-        onBlur={(event) => {props.percentageHandler(Number(event.target.value).toFixed(2))}}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">%</InputAdornment>,
-        }}
-      />
-      <PaymentMethod paymentMethod={props.paymentMethod} paymentMethodHandler={props.paymentMethodHandler}/>
-      <ConvertButton convertClicked={props.convertClicked} convertClickedHandler={props.convertClickedHandler} nameButton={"Converter"}/>
-      </form>
+        <FormBox>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={4} lg={2} style={{paddingRight: "30px"}}>
+
+            <GreenTextField
+              variant="outlined"
+              margin="normal"
+              name="dollarAmount"
+              label="Dólar"
+              id="dollarAmount"
+              autoFocus
+              required
+              type="number"
+              value={props.dollarAmount}
+              onChange={(event) => { props.dollarAmountHandler(event.target.value) }}
+              onBlur={(event) => { props.dollarAmountHandler(Number(event.target.value).toFixed(2)) }}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">$</InputAdornment>
+              }}
+            />
+          </Grid>
+
+            
+          <Grid item xs={12} sm={12} md={4} lg={2} style={{paddingRight: "30px"}}>
+            <GreenTextField
+              variant="outlined"
+              margin="normal"
+              name="taxState"
+              required
+              label="Taxa do estado"
+              id="password"
+              type="number"
+              value={props.percentage}
+              onChange={(event) => { props.percentageHandler(event.target.value) }}
+              onBlur={(event) => { props.percentageHandler(Number(event.target.value).toFixed(2)) }}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">%</InputAdornment>,
+              }}
+            />
+          </Grid>
+        </Grid>
+      </FormBox>
+        <PaymentMethod paymentMethod={props.paymentMethod} paymentMethodHandler={props.paymentMethodHandler} />
+        <Grid>
+        <ConvertButton btnClicked={props.convertClicked} btnkClickedHandler={props.convertClickedHandler} btnName={"Converter"} />
+        </Grid>
+        
     </>
   )
 }
---
+++ src/Components/DollarForm/Style.ts
@@ -0,0 +1,4 @@
+import styled from "styled-components"
+export const FormBox = styled.div`
+  padding-bottom: 2.5rem;
+`;
