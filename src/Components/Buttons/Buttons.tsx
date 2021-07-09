9468745be75fa651144e703a454c75a5cf1d6048
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-08 23:06:00 -0400

    Estilizado os Botões

--- src/Components/Buttons/Buttons.tsx
+++ src/Components/Buttons/Buttons.tsx
@@ -1,17 +1,56 @@
 import { Button } from '@material-ui/core';
+import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
+import { withStyles } from '@material-ui/core/styles';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 interface iProps{
     btnName: string
     btnClicked:boolean,
-    btnkClickedHandler: Function
+    btnkClickedHandler: Function,
+
 }
+
+const ConvertButtonStyled = withStyles(() => ({
+    root: {
+      color: "#fff",
+      backgroundColor: "#00AB63",
+      textTransform: "capitalize",
+      padding: "16px",
+      fontSize: "16px",
+      '&:hover': {
+        backgroundColor: "#00AB63",
+      },
+    },
+  }))(Button);
+
+  const BackButtonStyled = withStyles(() => ({
+    root: {
+      color: "#2E3742",
+      backgroundColor: "#fff",
+      textTransform: "capitalize",
+      padding: "16px",
+      fontSize: "16px",
+      '&:hover': {
+        backgroundColor: "#ececec",
+      },
+    },
+  }))(Button);
+
 const BackButton = (props:iProps) =>{
     return(
-        <Button onClick={() => props.btnkClickedHandler(props.btnClicked)}>{props.btnName}</Button>
+        <BackButtonStyled 
+        variant="contained"
+        size="large"
+        startIcon={<ArrowBackIcon />}onClick={() => {document.location.reload()}}>{props.btnName}</BackButtonStyled>
     )
 }
 const ConvertButton = (props:iProps) =>{
     return(
-        <Button onClick={() => props.btnkClickedHandler(!props.btnClicked)}>{props.btnName}</Button>
+        <ConvertButtonStyled
+        variant="contained"
+        size="large"
+        startIcon={<SwapHorizIcon />}
+        onClick={() => props.btnkClickedHandler(!props.btnClicked)}>{props.btnName}</ConvertButtonStyled>
+
     )
 }
 export { BackButton, ConvertButton}
