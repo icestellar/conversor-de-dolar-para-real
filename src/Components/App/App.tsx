36d3103928eeb255224fbdf8872dd72a0c708bc5
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-11 18:55:57 -0400

    Readequada as referências aos componentes

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,47 +1,54 @@
-import {useEffect, useState} from 'react';
-import GlobalStyle from '../../globalStyle';
-import Header from '../Header/Header';
+import { useEffect, useState } from 'react';
+import Header from '../Header';
 import FetchUSDData from "../../Functions/API/FetchUSDdata";
-import { formatDate } from '../../Functions/Utils/FormatData';
-import DollarForm from '../DollarForm/DollarForm';
-import Result from '../Result/Result';
-
+import { FormatDate } from '../../Functions/Utils/FormatDate';
+import DollarForm from '../DollarForm';
+import Result from '../Result';
+import { CheckIfInputIsValid } from '../../Functions/Utils/TextValidation';
+import GlobalStyle from '../GlobalStyle';
 function App() {
   const [day, setDay] = useState("")
-  const [hour, setHour] =  useState("")
+  const [hour, setHour] = useState("")
   const [dollarCurrentValue, setdollarCurrentValue] = useState(0)
   const [dollarAmount, setdollarAmount] = useState("");
   const [percentage, setPercentage] = useState("");
   const [paymentMethod, setPaymentMethod] = useState("dinheiro");
   const [convertClicked, setConvertClicked] = useState(true);
+  const [enableButton, setEnableButton] = useState(true);
 
-useEffect(() => {
-  FetchUSDData().then( (apiData) => {
-    setdollarCurrentValue(apiData.ask)
-    setDay(formatDate(apiData.create_date).day)
-    setHour(formatDate(apiData.create_date).hour)
-  });
-}, []);
+  useEffect(() => {
+    FetchUSDData().then((apiData) => {
+      setdollarCurrentValue(apiData.ask || 0)
+      setDay(FormatDate(apiData.create_date).day)
+      setHour(FormatDate(apiData.create_date).hour)
+    });
+  }, []);
+
+  useEffect(() => {
+    setEnableButton(!CheckIfInputIsValid(dollarAmount, percentage))
+  }, [dollarAmount, percentage]);
 
   return (
     <div className="App">
       <GlobalStyle />
-      <Header day={day} hour={hour}/>
+      <Header day={day} hour={hour} />
       <div>
-        {(convertClicked) ? <DollarForm dollarAmount={dollarAmount} 
-                  dollarAmountHandler={setdollarAmount} 
-                  percentage={percentage} 
-                  percentageHandler={setPercentage} 
-                  paymentMethod={paymentMethod} 
-                  paymentMethodHandler={setPaymentMethod} 
-                  convertClicked={convertClicked}
-                  convertClickedHandler={setConvertClicked}/> : 
-
-                  <Result dollarCurrentValue={dollarCurrentValue} 
-                          dollarAmount={dollarAmount} 
-                          percentage={percentage}
-                          paymentMethod={paymentMethod}
-                          backClicked={!convertClicked} backClickedHandler={setConvertClicked}/>}
+        {(convertClicked) ? <DollarForm dollarAmount={dollarAmount}
+          dollarAmountHandler={setdollarAmount}
+          percentage={percentage}
+          percentageHandler={setPercentage}
+          paymentMethod={paymentMethod}
+          paymentMethodHandler={setPaymentMethod}
+          convertClicked={convertClicked}
+          convertClickedHandler={setConvertClicked}
+          enabledButton={enableButton} />
+          :
+          <Result dollarCurrentValue={dollarCurrentValue}
+            dollarAmount={dollarAmount}
+            percentage={percentage}
+            paymentMethod={paymentMethod}
+            backClicked={!convertClicked}
+            backClickedHandler={setConvertClicked} />}
       </div>
     </div>
   );
