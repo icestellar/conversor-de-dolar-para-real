df7e4c7e0c5a993ffc4428b25aece5df06ef74f0
icestellar <56412794+icestellar@users.noreply.github.com>
2021-07-12 19:59:56 -0400

    Update index.tsx

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import GlobalStyle from '../GlobalStyle';
 
 //Functions
-import FetchUSDData from "../../Functions/API/FetchDolarData";
+import FetchDolarData from "../../Functions/API/FetchDolarData";
 import { FormatDate } from '../../Functions/Utils/FormatDate';
 
 //Components
