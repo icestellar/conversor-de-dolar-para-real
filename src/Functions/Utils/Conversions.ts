19db758ae15ce22a0552f3b8c4246b6c9da31076
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-12 18:35:25 -0400

    Ajustado os métodos de conversão

--- src/Functions/Utils/Conversions.ts
+++ src/Functions/Utils/Conversions.ts
@@ -14,10 +14,10 @@ const TaxDetails = (dollarAmount: any, percentage: any, dollarCurrentValue: any,
     var dollarValueWithTax = ((1 + (percentage / 100)) * dollarAmount)
     var realValueWithoutTax = (dollarAmount * dollarCurrentValue * (1 + (percentage / 100)))
     return {
-        paymentMethod: paymentMethod == "dinheiro" ? 1.10 : 6.38,
+        paymentMethod: paymentMethod === "dinheiro" ? 1.10 : 6.38,
         dollarValueWithTax: dollarValueWithTax.toFixed(2),
         realValueWithoutTax: realValueWithoutTax.toFixed(2),
-        paymentMethodName: paymentMethod == "dinheiro" ? "Dinheiro" : "Cartão"
+        paymentMethodName: paymentMethod === "dinheiro" ? "Dinheiro" : "Cartão"
     }
 }
 
--- src/Functions/Utils/TextValidation.ts
+++ src/Functions/Utils/TextValidation.ts
@@ -2,7 +2,7 @@ const CheckIfInputIsValid = (value:string, percentage:string) => {
     return Number(value) > 0  && Number(percentage) > 0
 }
 const ReturnFormatted = (text:string) => {
-    return Number(text).toFixed(2) != "0.00" ? Number(text).toFixed(2) : "" 
+    return Number(text).toFixed(2) !== "0.00" ? Number(text).toFixed(2) : "" 
 }
 
 const ReturnPositive = (text:string) =>{
@@ -10,7 +10,7 @@ const ReturnPositive = (text:string) =>{
 }
 
 const ReturnValidPercentage = (text:string) => {
-    if(Number(text) <= 0 ) return ""
+    if(Number(text) < 0 ) return ""
     if(Number(text) >= 10000) return "9999"
     return text
 }
